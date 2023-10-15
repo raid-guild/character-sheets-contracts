3bc791c09a1f15baf8dc12087fdecebcaec9a8d1
MrDeadCe11 <mister.deadce11@gmail.com>
2023-10-14 19:41:25 -0500

    ClassLevelAdaptor.t.sol refactored

--- test/ClassLevelAdaptor.t.sol
+++ test/ClassLevelAdaptor.t.sol
@@ -2,37 +2,36 @@
 pragma solidity ^0.8.19;
 pragma abicoder v2;
 
-// //solhint-disable
+//solhint-disable
 
-// import "forge-std/Test.sol";
+import "forge-std/Test.sol";
 
-// import "../src/implementations/ItemsImplementation.sol";
-// import "../src/lib/Structs.sol";
-// import "../src/lib/Errors.sol";
-// import "./helpers/SetUp.sol";
-// import {Contracts} from "./helpers/Contracts.sol";
+import "../src/implementations/ItemsImplementation.sol";
+import "../src/lib/Structs.sol";
+import "../src/lib/Errors.sol";
+import "./setup/SetUp.sol";
 
-// contract ClassLevelAdaptorTest is Test, SetUp, Contracts {
-//     function testSupportsInterface() public {
-//         assertTrue(classLevels.supportsInterface(0x01ffc9a7));
-//     }
+contract ClassLevelAdaptorTest is SetUp {
+    function testSupportsInterface() public {
+        assertTrue(deployments.classLevels.supportsInterface(0x01ffc9a7));
+    }
 
-//     function testLevelRequirementsMet() public {
-//         vm.expectRevert();
-//         vm.startPrank(npc1);
-//         classLevels.levelRequirementsMet(npc1, 1);
+    function testLevelRequirementsMet() public {
+        vm.expectRevert();
+        vm.startPrank(accounts.character1);
+        deployments.classLevels.levelRequirementsMet(accounts.character1, 1);
 
-//         classes.claimClass(0);
-//         assertFalse(classLevels.levelRequirementsMet(npc1, 0), "should not be met");
-//         vm.stopPrank();
-//         uint256 expAmount = 301 * 10 ** 18;
-//         vm.prank(admin);
-//         experience.dropExp(npc1, expAmount);
+        deployments.classes.claimClass(0);
+        assertFalse(deployments.classLevels.levelRequirementsMet(accounts.character1, 0), "should not be met");
+        vm.stopPrank();
+        uint256 expAmount = 301 * 10 ** 18;
+        vm.prank(accounts.dungeonMaster);
+        deployments.experience.dropExp(accounts.character1, expAmount);
 
-//         assertEq(experience.balanceOf(npc1), expAmount, "incorrect exp amount");
+        assertEq(deployments.experience.balanceOf(accounts.character1), expAmount, "incorrect exp amount");
 
-//         vm.prank(npc1);
+        vm.prank(accounts.character1);
 
-//         assertTrue(classLevels.levelRequirementsMet(npc1, 0), "should be met");
-//     }
-// }
+        assertTrue(deployments.classLevels.levelRequirementsMet(accounts.character1, 0), "should be met");
+    }
+}
