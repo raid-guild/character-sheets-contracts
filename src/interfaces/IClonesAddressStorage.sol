1ea4f7be8fddde84463fd73288d920442b5a13fe
MrDeadCe11 <mister.deadce11@gmail.com>
2023-11-01 14:03:24 -0500

    added experience functions to Iexperience.sol

--- src/interfaces/IClonesAddressStorage.sol
+++ src/interfaces/IClonesAddressStorage.sol
@@ -3,12 +3,20 @@ pragma solidity ^0.8.20;
 
 interface IClonesAddressStorage {
     function initialize(bytes calldata encodedAddresses, bytes calldata encodedAdaptorAddresses) external;
+
     function characterSheets() external pure returns (address);
+
     function items() external pure returns (address);
+
     function classes() external pure returns (address);
+
     function experience() external pure returns (address);
+
     function characterEligibilityAdaptor() external pure returns (address);
+
     function classLevelAdaptor() external pure returns (address);
+
     function itemsManager() external pure returns (address);
+
     function hatsAdaptor() external pure returns (address);
 }
