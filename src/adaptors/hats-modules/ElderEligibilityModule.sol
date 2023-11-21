9734595ce3df04ecf1bf352a42c813ed8db66fb7
MrDeadCe11 <mister.deadce11@gmail.com>
2023-11-21 11:32:22 -0600

    fixed linter errors and snapshot hopefully.

--- src/adaptors/hats-modules/ElderEligibilityModule.sol
+++ src/adaptors/hats-modules/ElderEligibilityModule.sol
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
-
+//solhint-disable
 // import {console2} from "forge-std/Test.sol"; // remove before deploy
+
 import {HatsEligibilityModule, HatsModule} from "hats-module/HatsEligibilityModule.sol";
 import {IERC1155} from "@openzeppelin-contracts/contracts/token/ERC1155/IERC1155.sol";
 import {ICharacterSheets} from "../../interfaces/ICharacterSheets.sol";
